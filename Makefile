e7d3f493865323de585b681380c61caebb24be78
unknown <joshapennington20@gmail.com>
2021-04-25 22:21:33 -0700

    Added an executable to test functions (yes I know I'm on master branch)

---
+++ Makefile
@@ -0,0 +1,2 @@
+tt: src/main.cpp src/player.cpp src/dice.cpp
+	g++ -o tt src/main.cpp
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "dice.hpp"
+#include "player.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"It runs"<<endl;
+    return 0;
+}
